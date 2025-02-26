aae7326c1ad0ae9f907542689dc0d5290ed167bd
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2025-02-26 20:31:01 +0100

    Feat: Añadiendo dependencias

---
+++ src/test/java/com/eventos/servicio_eventos/ServicioEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.servicio_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
