a184d592f9eeabb0e64109fa027b8c42a328d9e9
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2025-03-08 22:13:35 +0100

    Fix: Event entity

--- servicio-eventos-domain/src/main/java/com/domain/models/EventModel.java
+++ servicio-eventos-domain/src/main/java/com/domain/models/EventModel.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EventModel {
-    private Integer id;
+    private Long id;
     private String name;
     private String description;
     private String date;
--- servicio-eventos-infrastructure/src/main/java/com/infrastructure/entities/Event.java
+++ servicio-eventos-infrastructure/src/main/java/com/infrastructure/entities/Event.java
@@ -20,7 +20,7 @@ public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
     private String name;
     private String description;
     private String date;
